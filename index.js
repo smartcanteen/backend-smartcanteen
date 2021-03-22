bf55109125e40a3a11b5081ee618adfdf18e0006
alifnaufalyasin <alifnaufal171199@gmail.com>
2021-03-22 13:31:11 +0700

    Update data cors and schema

--- index.js
+++ index.js
@@ -17,22 +17,27 @@ var allowedOrigins = [
   "http://localhost"
 ]
 
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: function (origin, callback) {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       var msg =
+//       "The CORS policy for this site does not " +
+//       "allow access from the specified Origin.";
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//     }
+//   })
+// )
 app.use(
   cors({
     credentials: true,
-    origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      var msg =
-      "The CORS policy for this site does not " +
-      "allow access from the specified Origin.";
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-    }
+    origin: true
   })
 )
-
 // app.use(cors())
 
 // middleware
