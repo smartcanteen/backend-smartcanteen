627fef499153181e948adb58a4a658ec9e4c1289
alifnaufalyasin <alifnaufal171199@gmail.com>
2021-04-06 14:52:35 +0700

    update admin schema for data input

--- schema/admin.js
+++ schema/admin.js
@@ -1,8 +1,8 @@
 const Joi = require('joi')
 
 const loginAdmin = Joi.object({
-    email : Joi.string().email().required(),
-    password : Joi.string().required().min(6),
+    email : Joi.string().email().message('masukkan email dengan benar').required(),
+    password : Joi.string().min(6).message('password minimal 6 karakter').pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).message('Silahkan gunakan kombinasi besar kecil dan angka').required(),
 })
 
 const registerAdmin = Joi.object({
@@ -10,7 +10,7 @@ const registerAdmin = Joi.object({
     last_name : Joi.string().min(3).message('masukkan nama minimal 3 huruf').required(),
     email : Joi.string().email().message('masukkan email dengan benar').required(),
     no_telp : Joi.string().min(9).message('nomor telepon harus lebih dari 9').max(12).message('nomor telepon tidak boleh lebih dari 12').required(),
-    password : Joi.string().min(6).message('Silahkan gunakan kombinasi caps dan angka min 6').required()
+    password : Joi.string().min(6).message('password minimal 6 karakter').pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).message('Silahkan gunakan kombinasi besar kecil dan angka').required()
 })
 
 module.exports = {
