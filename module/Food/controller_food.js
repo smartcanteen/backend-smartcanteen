70f783d3fecb928f3c678b72b57abfc9b2cde40b
alifnaufalyasin <alifnaufal171199@gmail.com>
2021-04-06 14:52:03 +0700

    Add food module

--- module/Food/controller_food.js
+++ module/Food/controller_food.js
@@ -1,29 +1,60 @@
 // const { signUser } = require('../../middleware/auth')
 // const { encryptPass, isValid } = require('../../helpers/encrypt')
 const Food = require('../../models/food')
-require('dotenv').config()
+const Seller = require('../../models/seller')
 
-async function tambahMakanan(req,res) {
-  let payload = req.body
-  let food = await Food.findOne({where : {nama: payload.nama} })  
-  if (food) return res.sendError({},'Akun sudah ada, silahkan gunakan email lainnya',401)
-  if (!req.user.isSeller) return res.sendError({},'Anda bukan penjual, silahkan kontak admin untuk lebih lanjut',401)
-  food = new Food(payload)
-  await food.save()
-  res.sendResponse(food,'Makanan berhasil ditambahkan',201)
-}
+class controller_food{
+  constructor(){
+  }
 
-async function detailMakanan(req,res) {
-  const id = req.params.id
-  const food = await Food.findByPk(id)
-  if (!food) return res.sendError({},'Makanan tidak ditemukan!',401)
-  else {
-    let data = 
-    res.sendResponse(data,'Sukses Login',200)
+  async tambahMakanan(req,res) {
+    let payload = req.body
+    // let food = await Food.findOne({where : {nama: payload.nama} })  
+    // if (food) return res.sendError({},'Akun sudah ada, silahkan gunakan email lainnya',401)
+    if (!req.user.isSeller) return res.sendError({},'Anda bukan penjual, silahkan kontak admin untuk lebih lanjut',401)
+    const {id_penjual} = req.user
+    let food = await Food.findOne({ where: {id_penjual, nama:payload.nama}, attributes: { exclude: ['deletedAt','updatedAt'] } })
+    if (food) return res.sendError({},'Makanan sudah pernah ditambahkan',401)
+    const seller = await Seller.findByPk(id_penjual)
+    food = new Food(payload)
+    await food.setSeller(seller)
+    await food.save()
+    return res.sendResponse(food,'Makanan berhasil ditambahkan',201)
+  }
+
+  async getAllMakanan(req,res){
+    const {id_penjual, isAdmin} = req.user
+    // const seller = await Seller.findByPk(id_penjual)
+    let food = await Food.findAll({ attributes: { exclude: ['deletedAt','updatedAt'] }, include: { model: Seller, attributes: { exclude: ['password','deletedAt','updatedAt'] } } })
+    if (!isAdmin) food = await Food.findAll({ where: {id_penjual}, attributes: { exclude: ['deletedAt','updatedAt'] }, include: { model: Seller, attributes: { exclude: ['password','deletedAt','updatedAt'] } } })
+    return res.sendResponse(food,'Makanan berhasil ditambahkan',201)
+  }
+
+  async detailMakanan(req,res) {
+    const id_makanan = req.params.id
+    const food = await Food.findOne({ where: {id_makanan}, attributes: { exclude: ['deletedAt','updatedAt'] }, include: { model: Seller, attributes: { exclude: ['password','deletedAt','updatedAt'] } } })
+    if (!food) return res.sendError({},'Makanan tidak ditemukan!',401)
+    return res.sendResponse(food,'Sukses Login',200)
+  }
+
+  async updateMakanan(req,res) {
+    const id_makanan = req.params.id
+    const payload = req.body;
+    const food = await Food.findOne({ where: {id_makanan}, attributes: { exclude: ['deletedAt','updatedAt'] }, include: { model: Seller, attributes: { exclude: ['password','deletedAt','updatedAt'] } } })
+    if (!food) return res.sendError({},'Makanan tidak ditemukan!',401)
+    if(!req.user.isAdmin && (food.id_penjual !== req.user.id_penjual)) return res.sendError({},'Maaf anda tidak memiliki akses ini',401)
+    for (const key in payload){
+      food[key] = payload[key]
+    }
+    await food.save()
+    return res.sendResponse(food,'Makanan berhasil diupdate',200)
   }
 }
 
-module.exports = {
-  tambahMakanan,
-  detailMakanan
-}
+module.exports = controller_food
+
+
+// module.exports = {
+//   tambahMakanan,
+//   detailMakanan
+// }
--- module/Food/routes_food.js
+++ module/Food/routes_food.js
@@ -2,21 +2,27 @@ const router = require("express-promise-router")()
 
 // validation
 const { validateBody } = require("../../helpers/validator/validateBody")
-const foodController = require("./controller_food")
-// const foodSchemas = require("../../schema/food")
+const controller_food = require("./controller_food")
+const foodSchemas = require("../../schema/food")
 const { authenticateToken } = require("../../middleware/auth")
+const foodController = new controller_food()
 
-router.route('/:id')
-  .get(
-    authenticateToken,
-    foodController.detailMakanan
-  )
-
-router.route("/add")
+router
+  .route("/add")
   .post(
     authenticateToken,
-    validateBody(sellerSchemas.registerSeller),
+    validateBody(foodSchemas.tambahMakanan),
     foodController.tambahMakanan
   )
+router.route("/all").get(authenticateToken, foodController.getAllMakanan)
 
+router
+  .route("/update/:id")
+  .post(
+    authenticateToken,
+    validateBody(foodSchemas.updateMakanan),
+    foodController.updateMakanan
+  )
+
+router.route("/:id").get(authenticateToken, foodController.detailMakanan)
 module.exports = router
