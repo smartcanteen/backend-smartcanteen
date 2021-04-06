4b0f7b9b6cc2e0c0f136c7464e242cf87e761b3b
alifnaufalyasin <alifnaufal171199@gmail.com>
2021-04-06 14:52:50 +0700

    update seller schema for data input

--- schema/seller.js
+++ schema/seller.js
@@ -10,7 +10,7 @@ const registerSeller = Joi.object({
     last_name : Joi.string().min(3).required(),
     email : Joi.string().email().message('masukkan email dengan benar').required(),
     no_telp : Joi.string().min(9).message('nomor telepon harus lebih dari 9').max(12).message('nomor telepon tidak boleh lebih dari 12').required(),
-    password : Joi.string().required().min(6),
+    password : Joi.string().min(6).message('password minimal 6 karakter').pattern(new RegExp('^((?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$|penjual)')).message('Silahkan gunakan kombinasi karakter besar kecil, angka dan symbol').required(),
 })
 
 const updateSeller = Joi.object({
@@ -18,7 +18,7 @@ const updateSeller = Joi.object({
     last_name : Joi.string().min(3),
     email : Joi.string().email().message('masukkan email dengan benar'),
     no_telp :Joi.string().min(9).message('nomor telepon harus lebih dari 9').max(12).message('nomor telepon tidak boleh lebih dari 12'),
-    password : Joi.string().min(6),
+    password : Joi.string().min(6).message('password minimal 6 karakter').pattern(new RegExp('^((?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$|penjual)')).message('Silahkan gunakan kombinasi karakter besar kecil, angka dan symbol'),
 })
 
 module.exports = {
