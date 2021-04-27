db88dbcb7e36c9c12dbfe219ad43de149c75417f
alifnaufalyasin <alifnaufal171199@gmail.com>
2021-04-27 21:07:48 +0700

    bulk update, add order module and add tenant for seller

--- config/database.js
+++ config/database.js
@@ -1,38 +1,11 @@
-// const Sequelize = require("sequelize")
-// require("dotenv").config()
-// const db = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USERNAME,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: process.env.DB_HOST,
-//     dialect: "mysql",
-//     dialectOptions: {
-//       dateStrings: true,
-//       typeCast(field, next) {
-//         // for reading from database
-//         if (field.type === "DATETIME") {
-//           return field.string()
-//         }
-//         return next()
-//       },
-//     },
-//     // for writing to database
-//     timezone: "+07:00",
-//   },
-// )
-
-// module.exports = db
-
-
 const Sequelize = require("sequelize")
 require("dotenv").config()
 const db = new Sequelize(
-  process.env.DB_NAME_DEV,
-  process.env.DB_USERNAME_DEV,
-  process.env.DB_PASSWORD_DEV,
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST_DEV,
+    host: process.env.DB_HOST,
     dialect: "mysql",
     dialectOptions: {
       dateStrings: true,
@@ -50,3 +23,30 @@ const db = new Sequelize(
 )
 
 module.exports = db
+
+
+// const Sequelize = require("sequelize")
+// require("dotenv").config()
+// const db = new Sequelize(
+//   process.env.DB_NAME_DEV,
+//   process.env.DB_USERNAME_DEV,
+//   process.env.DB_PASSWORD_DEV,
+//   {
+//     host: process.env.DB_HOST_DEV,
+//     dialect: "mysql",
+//     dialectOptions: {
+//       dateStrings: true,
+//       typeCast(field, next) {
+//         // for reading from database
+//         if (field.type === "DATETIME") {
+//           return field.string()
+//         }
+//         return next()
+//       },
+//     },
+//     // for writing to database
+//     timezone: "+07:00",
+//   },
+// )
+
+// module.exports = db
