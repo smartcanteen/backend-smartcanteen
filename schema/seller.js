29748669274fa5c5eb1a3c5cb93ceef0069c413e
alifnaufalyasin <alifnaufal171199@gmail.com>
2021-06-13 22:30:53 +0700

    bugfix

--- schema/seller.js
+++ schema/seller.js
@@ -33,8 +33,7 @@ const updateSeller = Joi.object({
   first_name: Joi.string().min(3),
   last_name: Joi.string().min(3),
   email: Joi.string().email().message("masukkan email dengan benar"),
-  no_telp: Joi
-    .number()
+  no_telp: Joi.string()
     .min(9)
     .message("nomor telepon harus lebih dari 9")
     .max(12)
